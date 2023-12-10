deb7da6e9dc7922461cadf14019bfb987898563e
fahrimz <mzfahri620@gmail.com>
2023-12-10 17:29:21 +0700

    feat: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 style={{ color: '#535bf2' }}>Hello World</h1>
+      <div className="card">
+        <h4>@fahrimz</h4>
+      </div>
+    </>
+  )
+}
+
+export default App
